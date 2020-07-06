df96debb7a1d8b8932c9335133602d1dd200b58c
oryrosin <64798246+oryrosin@users.noreply.github.com>
2020-07-06 15:36:34 +0300

    adding tests- the begining

--- project/tests.py
+++ project/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+import json
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from project.models import ...
+from project.serializer import ...
