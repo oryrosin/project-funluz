90419cfb4f4d3838e13de397146351e22ecae748
Ory <orysjunk@gmail.com>
2020-07-27 22:29:44 +0300

    stam

--- project/test.py
+++ project/test.py
@@ -23,26 +23,40 @@ class OwnerTestCase(APITestCase):
         #self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def test_create_owner(self):
-        self.owner_name = "rakaz noar"
-        self.owner = Owner(name=self.owner_name)
+        #self.owner_name = "rakaz noar"
+        self.owner = Owner(name="rakaz")
         self.owner.save()
-        response = self.client.post("/owners/", {"name": self.owner_name})
+        response = self.client.post("/owners/", {"name": "rakaz"})
         self.assertEqual(201, response.status_code)
-
 
     def test_get_an_owner(self):
         #Test the api can get a given owners name.
-        self.owner_name = "rakaz noar"
-        self.owner = Owner(name=self.owner_name)
+        self.owner = Owner(name="rakaz")
         self.owner.save()
-        owner = Owner.objects.get(name= "rakaz noar")
+        owner = Owner.objects.get(name= "rakaz")
         response = self.client.get("/owners/{}".format(owner.id))
         self.assertEqual(200, response.status_code)
 
+    # def test_get_group(self):
+    #     # get one group
+    #     group = GroupF()
+    #
+    #     url = reverse('group-detail', kwargs={'pk': group.pk})
+    #     res = self.client.get(url)
+    #     content = json.loads(res.content)
+    #     self.assertEqual(res.status_code, 200)
+
+    # def test_get_groups(self):
+    #     # one group available
+    #     url = reverse('group-list')
+    #     res = self.client.get(url)
+    #     content = json.loads(res.content)
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(len(content['results']), 0)
+
 
     def test_update_owner(self):
-        self.owner_name = "rakaz noar"
-        self.owner = Owner(name=self.owner_name)
+        self.owner = Owner(name="rakaz")
         self.owner.save()
         response = self.client.put("/owners/", {"name": "new rakaz"})
         response_data = json.loads(response.content)
@@ -50,8 +64,7 @@ class OwnerTestCase(APITestCase):
         self.assertEqual(response_data.get("name"), owner.name)
 
     def test_owner_object_delete(self):
-        self.owner_name = "rakaz noar"
-        self.owner = Owner(name=self.owner_name)
+        self.owner = Owner(name="rakaz")
         self.owner.save()
-        response = self.client.delete("/owners/", {"name": self.owner_name})
+        response = self.client.delete("/owners/{}".format(self.owner.id))
         self.assertEqual(204, response.status_code)
